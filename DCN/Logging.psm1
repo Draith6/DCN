76459bf4592102b9245608da9d15a07630550155
Donnie Taylor <draith@outlook.com>
2016-10-26 16:24:33 -0500

    Add files via upload

---
+++ DCN/Logging.psm1
@@ -0,0 +1,48 @@
+﻿function Write-Log
+{
+   <#
+    .SYNOPSIS
+        Simple function to write to a log file
+    .DESCRIPTION
+        This function will write to a log file, pre-pending the date/time, level of detail, and supplied information
+    .PARAMETER text
+        This is the main text to log
+    .PARAMETER Level
+        INFO,WARN,ERROR,DEBUG
+    .PARAMETER Log
+        Name of the log file to send the data to.
+    .EXAMPLE
+        write-log -text "This is the main problem." -level ERROR -log c:\test.log
+    .FUNCTIONALITY
+        PowerShell Language
+    .NOTES
+        Created by Donnie Taylor.
+        Version 1.0     Date 4/5/2016
+    #>
+
+    Param (   
+        [Parameter(Mandatory=$true,position=0)]
+        [ValidateNotNull()]
+        [string]$text,
+        [Parameter(Mandatory=$true,position=1)]
+        [ValidateSet("INFO","WARN","ERROR","DEBUG")]
+        $level,
+        [Parameter(Mandatory=$true,position=2)]
+        $log
+        )
+    Write-Verbose "Log:  $log"
+    $date = (get-date).ToString()
+    if (Test-Path $log) 
+    { 
+        if ((Get-Item $log).length -gt 5mb)
+        {
+			$filenamedate = get-date -Format 'MM-dd-yy hh.mm.ss'
+			$archivelog = ($log + '.' + $filenamedate + '.archive').Replace('/', '-')
+            copy-item $log -Destination $archivelog
+            Remove-Item $log -force
+            Write-Verbose "Rolled the log."
+        } 
+    }
+    $line = $date + '---'+ $level + '---' + $text
+    $line |out-file -FilePath $log -Append
+}
--- DCN/dcn.psm1
+++ DCN/dcn.psm1
@@ -1,15 +1,13 @@
-﻿<#
+﻿
+<#
 	.SYNOPSIS
 		Finds Current DCN Controller
 	
 	.DESCRIPTION
-		Queries the DCN database and retrieves the current controller.  
-	
-	.PARAMETER DatabaseParameters
-		The Database parameters of the DCN database.  Should include the server name, the database name, and if integrated security should be used.
+		Queries the DCN config and retrieves the current controller.
 	
 	.EXAMPLE
-				PS C:\> Get-DCNController -DatabaseParameters $value1
+		PS C:\> Get-DCNController -DatabaseParameters $value1
 	
 	.NOTES
 		Additional information about the function.
@@ -17,102 +15,65 @@
 function Get-DCNController
 {
 	[CmdletBinding()]
-	param
-	(
-		[Parameter(Mandatory = $true)]
-		[ValidateNotNullOrEmpty()]
-		[array]$DatabaseParameters
-	)
+	param ()
 	
-	
+	$DCNConfigPath = (Get-ItemProperty HKLM:\SOFTWARE\DCN -Name DCNConfigPath).DCNConfigPath
+	Write-Output (Get-Content $DCNConfigPath\Members.cfg|ConvertFrom-Json).controller
 }
-<#
-	.SYNOPSIS
-		Creates a new DCN Database instance for new installs.
-	
-	.DESCRIPTION
-		This function will create a new DCN database instance that is essentially blank.  Requires that the user running the function have database create rights on the SQL server (parameter)
-	
-	.PARAMETER SqlServer
-		The FQDN of the SQL server that will host the new DCN instance.  SQL Availability Groups are supported.
-	
-	.PARAMETER SqlInstance
-		If using a non-default instance, specify the instance name here.  Otherwise leave blank to use default.
-	
-	.EXAMPLE
-				PS C:\> New-DCNDatabase -SqlServer 'Value1' -SqlInstance 'Value2'
-	
-	.NOTES
-		Additional information about the function.
-#>
-function New-DCNDatabase
-{
-	[CmdletBinding()]
-	param
-	(
-		[Parameter(Mandatory = $true,
-				   Position = 0)]
-		[ValidateNotNullOrEmpty()]
-		[string]$SqlServer,
-		[Parameter(Position = 1)]
-		[string]$SqlInstance
-	)
-	
-	#TODO: Place script here
-}
-<#
-	.SYNOPSIS
-		Test connection to the specified DCN database
-	
-	.DESCRIPTION
-		Will test connection to the specified DCN database.  Returns a true/false.  Requires that the user running the function have query access to the database.
-	
-	.PARAMETER Server
-		A description of the Server parameter.
-	
-	.PARAMETER Database
-		A description of the Database parameter.
-	
-	.PARAMETER Integrated
-		A description of the Integrated parameter.
-	
-	.EXAMPLE
-				PS C:\> Test-DBConnection -Server 'Value1' -Database 'Value2'
-	
-	.NOTES
-		Additional information about the function.
-#>
-function Test-DBConnection
-{
-	[CmdletBinding()]
-	param
-	(
-		[Parameter(Mandatory = $true,
-				   Position = 0)]
-		[string]$Server,
-		[Parameter(Mandatory = $true,
-				   Position = 1)]
-		[string]$Database,
-		[Parameter(Mandatory = $true,
-				   Position = 2)]
-		[bin]$Integrated
-	)
-	
-	#TODO: Place script here
-}
+
 function Get-DCNMembers
 {
 	[CmdletBinding()]
 	param ()
 	
-	#TODO: Place script here
+	$DCNConfigPath = (Get-ItemProperty HKLM:\SOFTWARE\DCN -Name DCNConfigPath).DCNConfigPath
+	Write-Output (Get-Content $DCNConfigPath\Members.cfg | ConvertFrom-Json).Members
 }
+<#
+	.SYNOPSIS
+		Adds a new DCN member to the pool
+	
+	.DESCRIPTION
+		Adds a new member to the DCN pool, which enables the machine to start performing tasks.  Once added, new machines will start performing tasks during the next run cycle.
+	
+	.PARAMETER computer
+		Name of the machine to be added to the DCN members array.
+	
+	.NOTES
+		Additional information about the function.
+#>
 function New-DCNMember
 {
-	[CmdletBinding()]
-	param ()
+	[CmdletBinding(PositionalBinding = $true)]
+	param
+	(
+		[Parameter(Mandatory = $true,
+				   ValueFromPipeline = $true,
+				   ValueFromPipelineByPropertyName = $true,
+				   ValueFromRemainingArguments = $true,
+				   Position = 0)]
+		[ValidateNotNullOrEmpty()]
+		[Alias('server', 'workstation')]
+		[string]$computer
+	)
 	
-	#TODO: Place script here
+BEGIN 
+{
+	#BeginBlock
+}
+PROCESS
+{
+	$DCNConfigPath = (Get-ItemProperty HKLM:\SOFTWARE\DCN -Name DCNConfigPath).DCNConfigPath
+	$DCNConfig = Get-Content $DCNConfigPath\Members.cfg | ConvertFrom-Json
+	[array]$DCNConfig.members += $computer
+	$DCNConfig | ConvertTo-Json | Out-File "$DCNConfigPath\Members.cfg" -Force
+	
+}
+END
+{
+	#EndBlock
+}
+	
 }
 function Get-DCNMember
 {
@@ -121,12 +82,50 @@ function Get-DCNMember
 	
 	#TODO: Place script here
 }
+
+<#
+	.SYNOPSIS
+		Creates a new DCNController and updates config
+	
+	.DESCRIPTION
+		Creates a new DCNController, effectively taking over control capabilities from all other DCN members, and using the current set of tasks as the source of record.
+	
+	.PARAMETER computer
+		Name of the computer that will become the new DCN Controller.  If this parameter is left blank, assume current machine.
+	
+	.NOTES
+		Additional information about the function.
+#>
 function New-DCNController
 {
-	[CmdletBinding()]
-	param ()
+	[CmdletBinding(PositionalBinding = $true)]
+	param
+	(
+		[Parameter(Mandatory = $false,
+				   ValueFromPipeline = $true,
+				   ValueFromPipelineByPropertyName = $true,
+				   ValueFromRemainingArguments = $true,
+				   Position = 0)]
+		[string]$computer = (resolve-dnsname 127.0.0.1).namehost
+	)
 	
-	#TODO: Place script here
+	BEGIN
+	{
+		#BeginBlock
+	}
+	PROCESS
+	{
+		if ($computer = (resolve-dnsname 127.0.0.1).namehost)
+		{
+			$DCNConfigFile = (Get-ItemProperty -Path HKLM:\SOFTWARE\DCN -Name DCNConfigPath).DCNConfigPath
+			$config = Get-Content $DCNConfigFile\config.cfg | ConvertFrom-Json
+			$members = Get-Content $config.membersfile
+		}
+	}
+	END
+	{
+		#EndBlock
+	}
 }
 function Add-DCNTask
 {
@@ -170,13 +169,36 @@ function Remove-DCNTask
 	
 	#TODO: Place script here
 }
+<#
+	.SYNOPSIS
+		Removes a DCN Member server
+	
+	.DESCRIPTION
+		A detailed description of the Remove-DCNMember function.
+	
+	.PARAMETER computer
+		A description of the computer parameter.
+	
+	.NOTES
+		Additional information about the function.
+#>
 function Remove-DCNMember
 {
 	[CmdletBinding()]
-	param ()
+	param
+	(
+		[Parameter(Mandatory = $true,
+				   Position = 0)]
+		[string]$computer
+	)
 	
-	#TODO: Place script here
-}
+	$DCNConfigPath = (Get-ItemProperty HKLM:\SOFTWARE\DCN -Name DCNConfigPath).DCNConfigPath
+	$DCNConfig = Get-Content $DCNConfigPath\Members.cfg | ConvertFrom-Json
+	[array]$DCNConfig.members = $DCNConfig.members -ne $computer
+	$DCNConfig | ConvertTo-Json | Out-File "$DCNConfigPath\Members.cfg" -Force
+	
+	#TODO:  Remove cache, remove scheduled tasks, etc....
+}	
 function Compare-DCNCache
 {
 	[CmdletBinding()]
@@ -213,27 +235,34 @@ function Install-DCN
 	[CmdletBinding()]
 	param ()
 	
-	$DCNPrompt = Read-Host -Prompt "Please enter the path to install the DCN.  Press enter to accept <C:\DCN>" 
+	$DCNPrompt = Read-Host -Prompt "Please enter install path DCN shoud use.  Press enter to accept <C:\DCN>" 
 	try
 	{
 		if ($DCNPrompt)
 		{
-			$DCNPath = $DCNConfigPrompt
+			$global:DCNPath = $DCNPrompt
 		}
 		else
 		{
-			$DCNPath = "C:\DCN"
+			$global:DCNPath = "C:\DCN"
 		}
 		New-Item -ItemType dir -Path $DCNPath -Force | Out-Null
 		New-Item -ItemType dir -Path $DCNPath\Config -Force | Out-Null
 		New-Item -ItemType dir -Path $DCNPath\Cache -Force | Out-Null
 		New-Item -ItemType dir -Path $DCNPath\Logs -Force | Out-Null
-		$global:installlog = "$DCNPath\install.log"
+		New-Item -Path HKLM:\Software -Name DCN –Force | Out-Null
+		New-ItemProperty -Path HKLM:\SOFTWARE\DCN -Name DCNPath -Value $DCNPath | Out-Null
+		New-ItemProperty -Path HKLM:\SOFTWARE\DCN -Name DCNConfigPath -Value $DCNPath\Config | Out-Null
+		New-ItemProperty -Path HKLM:\SOFTWARE\DCN -Name DCNCachePath -Value $DCNPath\Cache | Out-Null
+		New-ItemProperty -Path HKLM:\SOFTWARE\DCN -Name DCNLogsPath -Value $DCNPath\Logs | Out-Null
+		$global:installlog = "$DCNPath\Logs\install.log"
+		write-log -log $installlog -level INFO -text 'Installation of DCN Started'
 	}
 	catch
 	{
-		Write-Output "Error during installation. Error was $_"
+		Write-host "Error during installation. Error was $_"
 	}
+	
 	$config = @{
 		ConfigPath = "$DCNPath\Config"
 		CachePath = "$DCNPath\Cache"
@@ -243,14 +272,34 @@ function Install-DCN
 		TaskList = "$DCNPath\Config\Tasks.cfg"
 		MembersFile = "$DCNPath\Config\Members.cfg"
 	}
-	$config |ConvertTo-Json|Out-File "$DCNPath\Config\config.cfg" -Force
+	$hostname = (resolve-dnsname 127.0.0.1).namehost
+	$members = @{
+		Controller = "$hostname"
+		Members = "$hostname"
+	}	
+try
+	{
+		$config | ConvertTo-Json | Out-File "$DCNPath\Config\config.cfg" -Force
+		write-log -log $installlog -level INFO -text "Config file created at $DCNPath\Config\config.cfg"
+		$members | ConvertTo-Json | Out-File "$DCNPath\Config\Members.cfg" -Force
+		write-log -log $installlog -level INFO -text "Members file created at $DCNPath\Config\members.cfg"
+	}
+	catch
+	{
+		write-log -log $installlog -level ERROR -text "Config file creation failed.  Error is $_"
+	}
 }
 function Uninstall-DCN
 {
 	[CmdletBinding()]
 	param ()
 	
-	#TODO: Place script here
+	if ((Read-Host "Do you wish to uninstall DCN entirely - Y/N?") -eq 'y')
+	{
+		$DCNPath = (Get-ItemProperty -Path HKLM:\SOFTWARE\DCN -Name DCNPath).DCNPath
+		Remove-Item $DCNPath -Force -Recurse
+		Remove-Item HKLM:\SOFTWARE\DCN -Recurse -Force
+	}
 }
 function Repair-DCNMember
 {
